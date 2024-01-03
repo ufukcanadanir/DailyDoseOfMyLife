84c43cb307e8c1b4dca9745944d96e8030dfc35d
ufukcanadanir <ufukcanadanir@gmail.com>
2024-01-03 18:53:19 +0300

    Add project files.

---
+++ DailyDoseOfMyLife/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DailyDoseOfMyLife
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DailyDoseOfMyLife/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyDoseOfMyLife
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
